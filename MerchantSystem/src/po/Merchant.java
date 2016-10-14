27710b15f93b3499795afe6f04713619e8ece08f
hwong443 <hwong443@gmail.com>
2016-10-14 13:18:02 +0800

    MerchantSystem 0.1

---
+++ MerchantSystem/src/po/Merchant.java
@@ -0,0 +1,54 @@
+package po;
+
+public class Merchant {
+	private int id;
+	private String name;
+	private String age;
+	private String gender;
+	private String shopName;
+	private String shopAddress;
+	private String shopTelno;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopAddress() {
+		return shopAddress;
+	}
+	public void setShopAddress(String shopAddress) {
+		this.shopAddress = shopAddress;
+	}
+	public String getShopTelno() {
+		return shopTelno;
+	}
+	public void setShopTelno(String shopTelno) {
+		this.shopTelno = shopTelno;
+	}
+}
